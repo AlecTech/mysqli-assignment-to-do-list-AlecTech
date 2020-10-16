1f762c394a14697da6149a6b35e59a75206a0d87
AlecTech <eremeevoleg@gmail.com>
2020-10-16 16:45:19 -0600

    added dropdown menu with categories from SQL db, added due date inside form and added checkbox functionality to reflect on the web

--- README.md
+++ README.md
@@ -14,3 +14,16 @@ https://trello.com/b/93WzXLsd/alec-to-do-app-php-sql-full-stack
 
 https://github.com/codingWithElias/php-to-do-list/blob/master/css/style.css
 
+# if statment to check if checkbox is 1 or 0
+       if ($row['checked'] == 1)
+         {
+             $tasks .= '<input value="'.$row['checked'].'" type="checkbox" checked>';
+         }
+         else
+         {
+            $tasks .= '<input value="'.$row['checked'].'" type="checkbox">';
+         }
+
+<!-- instead of having this inside the sprintf 
+//   `<input value="%d" type="checkbox">
+                // $row['checked'],    `          -->
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require 'constants.php';
 
 $tasks = null;
+$cats = null;
 
 // Create connection
 $connection = new mysqli(HOST, USER, PASSWORD, DATABASE);
@@ -10,11 +11,36 @@ $connection = new mysqli(HOST, USER, PASSWORD, DATABASE);
 if ($connection->connect_error) {
 die('Connection failed: ' . $connection->connect_error);
 }
-
 // SET UP YOUR SELECT SQL STATEMENT
-$sql = 'SELECT * FROM todos';
+$sql = 'SELECT todos.id, todos.todoTitle, todos.date, todos.catID, todos.checked FROM todos';
+
+$category_id = 'SELECT categories.catID, categories.name FROM categories';
 // QUERY USING YOUR SQL STATEMENT
 $result = $connection->query($sql);
+
+$result_category = $connection->query($category_id);
+
+if($result_category->num_rows > 0) 
+{
+    while ($row_cat = $result_category->fetch_assoc()) 
+    {
+        echo '<pre>';
+        print_r($row_cat);
+        echo '</pre>';
+
+        $cats .= sprintf
+        ('
+            <option value="%s">
+            %s</option>
+
+        ',
+            $row_cat['catID'],
+            $row_cat['name']
+         );
+    }
+           
+
+}
 // TEST TO SEE IF YOUR QUERY RETURNED ANY RESULTS
 
 // if( $result->num_rows > 0) 
@@ -41,29 +67,49 @@ else
 {
     while( $row = $result->fetch_assoc() )
     {
-        // echo '<pre>';
-        // print_r($row);
-        // echo '</pre>';
-        // $tasks .= sprintf();
-        $tasks .= sprintf('
-            <p>%d</p>
-            <p>%s</p>
-            <p>%b</p>
-            <p>%d</p>
+        echo '<pre>';
+        print_r($row);
+        echo '</pre>';
+     
+        $tasks .= sprintf
+        ('
+            <div class="show-todo-list">
+                <div class="todo-item">
+                    <br>
+                    <h3>%d</h3>
+                    <h3>%s</h3>
+
+        
         ',
-        $row['id'],
-        $row['todoTitle'],
-        $row['checked'],
-        // $row['date'],
-        $row['catID'],
-     );
+                $row['id'],
+                $row['todoTitle'],
+            
+         );
+
+         if ($row['checked'] == 1)
+         {
+             $tasks .= '<input value="'.$row['checked'].'" type="checkbox" checked>';
+         }
+         else
+         {
+            $tasks .= '<input value="'.$row['checked'].'" type="checkbox">';
+         }
+
+         $tasks .= sprintf
+         ('
+
+                    <small>%s</small>
+                    <h3>%d</h3>
+                </div>
+            </div>
+
+        ',
+                $row['date'],
+                $row['catID'],
+
+        );
     }
 }
-// <p>%d</p>
-// <p>%s</p>
-// <p>%b</p>
-// <p><a>%04d-%02d-%02d</a> | 
-// <a>%d</a></p>
 
 
 $connection->close();
@@ -80,21 +126,30 @@ $connection->close();
 <body>
 <div class="main-section">
     <div class="add-section">
-        <form action="#">
+        <form action="#" method="GET" enctype="multipart/form-data">
             <input type="text"
                     name="title"
                     placeholder="Enter your task here..."/>
+            <lable for="due_date">Due Date</lable>
+            <input type="date" name="due_date" min="2020-09-01" max="2021-09-01">
+            <lable for="category"> Task Category</lable>
+            <select name="category" id="category">
+                <option value="">select category</option>
+                <?php echo $cats; ?>
+            </select>
             <button type="submit"> Add &nbsp; <span>&#43;</span></button>
+
         </form>
     </div>
     <div class="show-todo-list">
         <div class="todo-item">
             <br>
             <input type="checkbox">
-            <h2><?php echo $tasks;  ?></h2>
+            <h2></h2>
             <small>date created 1/1/2020</small>
         </div>
     </div>
+    <?php echo $tasks;  ?>
 </div>
     
     
