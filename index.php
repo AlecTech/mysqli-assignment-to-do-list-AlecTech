742d8e096668660187e315277cee82e9e8bc6587
AlecTech <eremeevoleg@gmail.com>
2020-10-15 21:47:45 -0600

    added constants.php and constants.example.php and ignoring original file with real constants for connection

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
+require 'constants.php';
 // Create connection
-$connection = new mysqli('127.0.0.1', 'root', '', 'to_do_list');
+$connection = new mysqli(HOST, USER, PASSWORD, DATABASE);
+
 // Did we have errors connecting?
 if ($connection->connect_error) {
 die('Connection failed: ' . $connection->connect_error);
 }
-echo 'Connected successfully. Now you can perform queries.';
+echo 'Connected successfully.';
 $connection->close();
 ?>
 
